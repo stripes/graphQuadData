e62626c1aa490e2704a877524c4186864a1a5f18
Josh Osborne <stripes@mac.com>
2024-04-12 09:08:42 -0400

    Initial Commit

---
+++ graphQuadData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  graphQuadData
+//
+//  Created by J Osborne on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ graphQuadData/graphQuadDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  graphQuadDataApp.swift
+//  graphQuadData
+//
+//  Created by J Osborne on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct graphQuadDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
